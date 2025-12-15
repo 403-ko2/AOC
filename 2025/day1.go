89794ca8eb84accc225bd7fc20d9a50b4cff3085
403-ko2 <177669720+403-ko2@users.noreply.github.com>
2025-12-15 13:02:00 -0500

    day-1 part II

--- 2025/day1.go
+++ 2025/day1.go
@@ -45,4 +45,77 @@ func main() {
 	}
 
 	fmt.Println("The real code is:", zeroCount)
+
+	//part II:
+
+	if err := method_0x434C49434B(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
+
+func method_0x434C49434B() error {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	position := 50
+	totalZeros := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0{
+			continue
+		}
+		
+		dir := line[0]
+		distance, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return fmt.Errorf("invalid", line)
+		}
+
+		position, hits := applyRotation(position, dir, distance)
+		totalZeros += hits
+
+	}
+	
+	if err := scanner.Err(); err != nil{
+		return err
+	}
+
+	fmt.Println("The actual password is:", totalZeros)
+	return nil
+}
+
+
+func applyRotation(position int, dir byte, distance int) (int, int) {
+	zeroCount := 0
+
+	for i := 0; i < distance; i++ {
+		switch dir {
+		case 'L':
+			position--
+			if position < 0 {
+				position = 99
+			}
+		case 'R':
+			position++
+			if position > 99 {
+				position = 0
+			}
+		default:
+			panic("invalid direction")
+		}
+
+		if position == 0 {
+			zeroCount++
+		}
+	}
+
+	return position, zeroCount
+}
+
+
+
+
+
+
