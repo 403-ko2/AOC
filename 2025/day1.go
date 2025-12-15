0d2fdb9d886b40562982493f1b2bcf20f6ad8160
403-ko2 <177669720+403-ko2@users.noreply.github.com>
2025-12-15 12:17:09 -0500

    catching up

--- 2025/day1.go
+++ 2025/day1.go
@@ -1,1 +1,48 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	position := 50
+	zeroCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			continue
+		}
+
+		dir := line[0]
+		distStr := line[1:]
+		distance, err := strconv.Atoi(distStr)
+
+		if err != nil {
+			fmt.Println("invalid rotation:", line)
+			return
+		}
+
+		if dir == 'L' {
+			position = (position - distance) % 100
+			if position < 0 {
+				position += 100
+			}
+		}else if dir == 'R' {
+			position = (position + distance) % 100
+		}else {
+			fmt.Println("Invalid Direction:", line)
+			return
+		}
+
+		if position == 0 {
+			zeroCount++
+		}
+	}
+
+	fmt.Println("The real code is:", zeroCount)
+}
